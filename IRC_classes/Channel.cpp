54e26ba1539bad0a927a7377cfa038f54b847ae8
Anna Valéria Makari <94723781+OnnaMcadva@users.noreply.github.com>
2025-03-19 23:00:39 +0100

    Update Channel.cpp

--- IRC_classes/Channel.cpp
+++ IRC_classes/Channel.cpp
@@ -2,6 +2,11 @@
 
 Channel::Channel(const std::string& n) : name(n) {}
 
+/* Функция `join` добавляет клиента (по его сокету) в список участников канала,
+если его там еще нет. 
+clientSocket — это идентификатор конкретного клиента, 
+подключенного к серверу, чтобы сервер знал, с кем он общается. */
+
 void Channel::join(int clientSocket) {
     bool clientExists = false;
     for (std::vector<int>::iterator it = members.begin(); it != members.end(); ++it) {
@@ -18,3 +23,4 @@ void Channel::join(int clientSocket) {
 std::string Channel::getName() const { return name; }
 
 std::vector<int> Channel::getMembers() const { return members; }
+
