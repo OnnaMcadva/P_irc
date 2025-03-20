48c843b528bdd5e8f55cf4572cb804d7c34aa1aa
Anna Valéria Makari <94723781+OnnaMcadva@users.noreply.github.com>
2025-03-20 18:50:59 +0100

    Update Config.hpp

--- IRC_classes/Config.hpp
+++ IRC_classes/Config.hpp
@@ -4,12 +4,12 @@
 
 class Config {
 public:
-    Config(int p, const std::string& pw);
+    Config(int p, const std::string& pw); // Конструктор с выбросом исключений
     int getPort() const;
     std::string getPassword() const;
+    void reconfigure(int newPort, const std::string& newPassword); // Новый метод для изменения конфигурации
 
 private:
     int port;
     std::string password;
 };
-
