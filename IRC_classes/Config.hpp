673f9d97609d94efc4ddbff60885ded103142780
Anna Valéria Makari <94723781+OnnaMcadva@users.noreply.github.com>
2025-03-20 19:44:51 +0100

    Update Config.hpp

--- IRC_classes/Config.hpp
+++ IRC_classes/Config.hpp
@@ -1,33 +1,15 @@
-#include "Config.hpp"
-#include <stdexcept>
-#include <iostream>
+#pragma once
 
-Config::Config(int p, const std::string& pw) {
-    if (p <= 0 || p > 65535) {
-        throw std::runtime_error("Port must be between 1 and 65535");
-    }
-    if (pw.empty()) {
-        throw std::runtime_error("Password cannot be empty");
-    }
-    port = p;
-    password = pw;
-}
+#include <string>
 
-int Config::getPort() const {
-    return port;
-}
+class Config {
+public:
+    Config(int p, const std::string& pw);
+    int getPort() const;
+    std::string getPassword() const;
+    void reconfigure(int newPort, const std::string& newPassword);
 
-std::string Config::getPassword() const {
-    return password;
-}
-
-void Config::reconfigure(int newPort, const std::string& newPassword) {
-    if (newPort <= 0 || newPort > 65535) {
-        throw std::runtime_error("New port must be between 1 and 65535");
-    }
-    if (newPassword.empty()) {
-        throw std::runtime_error("New password cannot be empty");
-    }
-    port = newPort;
-    password = newPassword;
-}
+private:
+    int port;
+    std::string password;
+};
